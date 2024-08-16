68b78949c306c5a265709045835085850d356bbc
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-08-16 23:33:56 +0500

    (added): webdriver manager

--- apps/scraper/utils/driver.py
+++ apps/scraper/utils/driver.py
@@ -6,9 +6,13 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
 options.add_argument("--headless")  # Run in headless mode
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")  # Disable GPU acceleration
 
 
@@ -22,7 +26,9 @@ class WebDriver:
         Web driver initialization
         """
         self.network = network
-        self.driver: webdriver.Chrome = webdriver.Chrome(options=options)
+        self.driver: webdriver.Chrome = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=options
+        )
 
     def get_html_content(self) -> str:
         """
