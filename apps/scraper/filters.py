977cfcc6bca231bdfc2e66a44d3c7cff620d1b27
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-08-23 00:51:39 +0500

    (fix): updated

--- apps/scraper/filters.py
+++ apps/scraper/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from scraper.models import Category, Comment, CommentStatuses, Product
+from scraper.models import Category, Comment, CommentStatuses, Product, ProductVariant
 
 
 class CategoryFilter(django_filters.FilterSet):
@@ -27,13 +27,24 @@ class ProductFilter(django_filters.FilterSet):
 
 
 class CommentsFilter(django_filters.FilterSet):
-    product_id = django_filters.NumberFilter(field_name="product_id")
+    product_id = django_filters.NumberFilter(method="filter_by_product_or_variant")
     source_id = django_filters.NumberFilter(field_name="source_id")
     rating = django_filters.NumberFilter(field_name="rating")
     status = django_filters.ChoiceFilter(
         field_name="status", choices=CommentStatuses.choices
     )
     user_id = django_filters.NumberFilter(field_name="user_id")
+
+    def filter_by_product_or_variant(self, queryset, name, value):
+        # First, try to filter comments by product_id
+        filtered_comments = queryset.filter(product_id=value)
+
+        # If no comments found, filter by ProductVariants related to the product
+        if not filtered_comments.exists():
+            product_variants = ProductVariant.objects.filter(product_id=value)
+            filtered_comments = queryset.filter(product__variants__in=product_variants)
+
+        return filtered_comments
 
     class Meta:
         model = Comment
