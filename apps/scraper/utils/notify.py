950920056a6fa7f35d6f4035e0e65eca7691868f
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-08-23 17:43:33 +0500

    (fix): updated

--- apps/scraper/utils/notify.py
+++ apps/scraper/utils/notify.py
@@ -13,5 +13,5 @@ def send_comment_notification(comment):
         send_email(
             users=[comment.user.email],
             subject=f"№{comment.pk} Статус обратной связи",
-            message=rf"Дорогой {str(comment.user.full_name if comment.user.full_name else comment.user.email)}!\n\nВаш отзыв №{comment.pk} не принят. Пожалуйста, дважды проверьте свой комментарий, а затем отправьте нам его еще раз для проверки.",
+            message=f"Дорогой {str(comment.user.full_name if comment.user.full_name else comment.user.email)}! Ваш отзыв №{comment.pk} не принят. Пожалуйста, дважды проверьте свой комментарий, а затем отправьте нам его еще раз для проверки.",
         )
