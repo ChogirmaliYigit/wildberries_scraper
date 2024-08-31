6275ec9486e660618cead69fd9d4d081da7e06fb
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-08-31 10:01:44 +0500

    (fix): updated

--- apps/scraper/urls.py
+++ apps/scraper/urls.py
@@ -19,13 +19,13 @@ urlpatterns = [
     path("product/<int:pk>", ProductDetailView.as_view(), name="product-detail"),
     path("comments", CommentsListView.as_view(), name="comments-list"),
     path("user-comments", UserCommentsListView.as_view(), name="user-comments-list"),
+    path(
+        "user-comments/<int:pk>",
+        CommentDetailView.as_view(),
+        name="user-comment-detail",
+    ),
     path("feedbacks", FeedbacksListView.as_view(), name="feedbacks-list"),
     path("user-feedbacks", UserFeedbacksListView.as_view(), name="user-feedbacks-list"),
-    path(
-        "user-feedbacks/<int:pk>",
-        CommentDetailView.as_view(),
-        name="user-feedback-detail",
-    ),
     path("favorites", FavoritesListView.as_view(), name="favorites-list"),
     path("like/<int:product_id>", LikeView.as_view(), name="like-a-product"),
     path(
