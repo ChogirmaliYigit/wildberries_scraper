3cb99e9e0e3035559dc79ba77e7b953fe1ddb90b
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-08-31 10:16:33 +0500

    (fix): updated

--- apps/scraper/utils/queryset.py
+++ apps/scraper/utils/queryset.py
@@ -1,12 +1,6 @@
 from django.db.models import DateTimeField, Exists, OuterRef
 from django.db.models.functions import Coalesce
-from scraper.models import (
-    Comment,
-    CommentFiles,
-    CommentStatuses,
-    Product,
-    ProductVariantImage,
-)
+from scraper.models import Comment, CommentStatuses, Product, ProductVariantImage
 
 
 def get_filtered_products():
@@ -21,9 +15,7 @@ def get_filtered_products():
 def get_filtered_comments(queryset=None):
     if queryset is None:
         queryset = Comment.objects.all()
-    queryset = queryset.annotate(
-        has_files=Exists(CommentFiles.objects.filter(comment=OuterRef("pk")))
-    ).filter(has_files=True, status=CommentStatuses.ACCEPTED, content__isnull=False)
+    queryset = queryset.filter(status=CommentStatuses.ACCEPTED, content__isnull=False)
     queryset = queryset.annotate(
         ordering_date=Coalesce(
             "source_date", "created_at", output_field=DateTimeField()
