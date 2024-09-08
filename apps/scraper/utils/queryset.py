9b6c7e88b55fe314c85070d0677df1016594c768
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-09-08 11:41:36 +0500

    (fix): product filter

--- apps/scraper/utils/queryset.py
+++ apps/scraper/utils/queryset.py
@@ -1,17 +1,25 @@
-from django.db.models import DateTimeField, Exists, OuterRef
+from django.db.models import DateTimeField, Exists, OuterRef, Q
 from django.db.models.functions import Coalesce
-from scraper.models import Comment, CommentStatuses, Product, ProductVariantImage
+from scraper.models import Comment, CommentStatuses, Product
 
 
 def get_filtered_products():
     return (
         Product.objects.annotate(
-            has_comments=Exists(Comment.objects.filter(product=OuterRef("pk"))),
-            has_images=Exists(
-                ProductVariantImage.objects.filter(variant__product=OuterRef("pk"))
-            ),
+            has_valid_comments=Exists(
+                Comment.objects.filter(
+                    product=OuterRef("pk"),
+                    content__isnull=False,  # Ensure content is not null
+                    content__gt="",  # Ensure content is not empty
+                ).filter(
+                    Q(files__isnull=False)  # Check for related CommentFiles
+                    | Q(
+                        file__isnull=False, file__gt=""
+                    )  # Check if the Comment has a valid file
+                )
+            )
         )
-        .filter(has_comments=True, has_images=True)
+        .filter(has_valid_comments=True)
         .distinct()
     )
 
@@ -20,7 +28,12 @@ def get_filtered_comments(queryset=None):
     if queryset is None:
         queryset = Comment.objects.all()
     queryset = queryset.filter(
-        status=CommentStatuses.ACCEPTED, content__isnull=False, content__gt=""
+        status=CommentStatuses.ACCEPTED,
+        content__isnull=False,
+        content__gt="",
+    ).filter(
+        Q(files__isnull=False)  # Check for related CommentFiles
+        | Q(file__isnull=False, file__gt="")  # Check if the Comment has a valid file
     )
     queryset = queryset.annotate(
         ordering_date=Coalesce(
