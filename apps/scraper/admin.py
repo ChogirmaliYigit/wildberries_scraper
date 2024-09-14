b0b4ed470b87c83c8926c898c557704f16c5648c
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-09-14 21:11:30 +0500

    (fix): changed permissions of category admin

--- apps/scraper/admin.py
+++ apps/scraper/admin.py
@@ -47,18 +47,28 @@ class CategoryAdmin(ModelAdmin):
     )
 
     def has_view_or_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
         return False
 
     def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
         return False
 
     def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
         return False
 
     def has_add_permission(self, request):
+        if request.user.is_superuser:
+            return True
         return False
 
     def has_module_permission(self, request):
+        if request.user.is_superuser:
+            return True
         return False
 
 
