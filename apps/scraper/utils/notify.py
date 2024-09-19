9c86bed54df7e2aa987d18cbf788bb1b3b81ed63
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-09-19 12:42:14 +0500

    (fix): comment status changed text

--- apps/scraper/utils/notify.py
+++ apps/scraper/utils/notify.py
@@ -5,18 +5,21 @@ def send_comment_notification(comment):
     from scraper.models import CommentStatuses
 
     if comment.user:
+        name = str(
+            comment.user.full_name if comment.user.full_name else comment.user.email
+        )
         if comment.status == CommentStatuses.NOT_ACCEPTED:
             reason = ""
             if comment.reason:
                 reason = f' по причине "{comment.reason}"'
             send_email(
                 users=[comment.user.email],
-                subject=f"№{comment.pk} Статус обратной связи",
-                message=f"Дорогой {str(comment.user.full_name if comment.user.full_name else comment.user.email)}! Ваш отзыв №{comment.pk} не принят{reason}. Пожалуйста, дважды проверьте свой комментарий, а затем отправьте нам его еще раз для проверки.",
+                subject="OZRO",
+                message=f"Уважаемый {name}, ваш отзыв отклонен{reason}, исправьте отзыв и отправьте еще раз.",
             )
         elif comment.status == CommentStatuses.ACCEPTED:
             send_email(
                 users=[comment.user.email],
-                subject=f"№{comment.pk} Accepted",
-                message="Otziv accepted",
+                subject="OZRO",
+                message=f"Уважаемый {name}, ваш отзыв одобрен, спасибо.",
             )
