cfd8ef1d47b06914073cf1efb41938c175618507
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-09-19 14:23:39 +0500

    (added): pagination_class

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,11 @@
+from core.pagination import CustomPageNumberPagination
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class BaseListCreateAPIView(generics.ListCreateAPIView):
+    pagination_class = CustomPageNumberPagination
+
     def get_serializer_context(self):
         context = super().get_serializer_context()
         context["request"] = self.request
@@ -16,6 +19,7 @@ class BaseListCreateAPIView(generics.ListCreateAPIView):
 
 class BaseListAPIView(generics.ListAPIView):
     permission_classes = (AllowAny,)
+    pagination_class = CustomPageNumberPagination
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
