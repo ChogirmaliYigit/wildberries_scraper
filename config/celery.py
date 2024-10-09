aa59172162938c54d0f627685a142f1b74977cbe
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-10-10 01:35:42 +0500

    (fixed): updated

--- config/celery.py
+++ config/celery.py
@@ -28,10 +28,10 @@ app.conf.beat_schedule = {
         "task": "update_products",
         "schedule": 60,
     },
-    "caching_products_and_comments": {
-        "task": "schedule_caching_for_products_and_comments",
-        "schedule": (settings.CACHE_DEFAULT_TIMEOUT * 3) - 100,
-    },
+    # "caching_products_and_comments": {
+    #     "task": "schedule_caching_for_products_and_comments",
+    #     "schedule": (settings.CACHE_DEFAULT_TIMEOUT * 3) - 100,
+    # },
 }
 app.conf.timezone = "Asia/Tashkent"
 
